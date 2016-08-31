7fc2a760982de19b765331ed96fbf6666a5adce6
Cyrill Leutwiler <bigcyrill@hotmail.com>
2016-08-31 23:01:38 +0200

    connect to database

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import redis, time
 from twython import Twython
 from twython import TwythonStreamer
+
+r = redis.StrictRedis(host='172.17.0.2', port=6379, password='letmeinplease', charset="utf-8", decode_responses=True)
+r.setnx('pk',0)
+r.setnx('keyword','unset')
 
 TAPIKEY = ''
 TAPISECRET = ''
@@ -12,6 +17,7 @@ TACCESSTOKENSECRET = ''
 class TDataStream(TwythonStreamer):
 	def on_success(self, data):
 		values = []
+		#todo: increment pk and add values to database
 		if 'text' in data:
 			txt = data['text'].encode('utf-8')
 			values.append('text:   ' + str(txt))
@@ -28,12 +34,22 @@ class TDataStream(TwythonStreamer):
 			country = data['place']['country']
 			values.append('fname:  ' + str(fullname))
 			values.append('cntry:  ' + str(country))
+		
 		for val in values:
 			print(val)
 
-		def on_error(self, status_code, data):
-			print(status_code)
+		if keyword != r.get('keyword'):
 			self.disconnect()
 
+	def on_error(self, status_code, data):
+		print(status_code)
+		self.disconnect()
+
 streamer = TDataStream(TAPIKEY, TAPISECRET, TACCESSTOKEN, TACCESSTOKENSECRET)
-streamer.statuses.filter(track = 'foo');
+
+while True:
+	keyword = r.get('keyword')
+	if keyword is not None and keyword != 'unset':
+		streamer.statuses.filter(track = keyword)
+	else:
+		time.sleep(0.5)
